20e742e6bf5ce560328c07c17122ee0a6f38f7be
Oterz <garelui@gmail.com>
2026-02-07 00:06:07 +0700

    robots

---
+++ app/robots.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/"],
+      },
+    ],
+    sitemap: "https://lenyoleny.ru/sitemap.xml",
+    host: "https://lenyoleny.ru",
+  };
+}
